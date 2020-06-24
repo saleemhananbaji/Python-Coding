d896beec4da09ed303a4d009b73f634b90b031f4
saleemhananbaji <53473626+saleemhananbaji@users.noreply.github.com>
2020-06-24 19:56:30 +0530

    Add files via upload

---
+++ Median_array.py
@@ -0,0 +1,49 @@
+from math import floor
+t=int(input())
+arr=[]
+n=int(input())
+for i in range(0,n):
+    ele=int(input())
+    arr.append(ele)
+arr=sorted(arr)
+mid=abs(int(len(arr)/2))
+if n%2==1:
+    arr5=arr[mid]
+   
+else:
+    arr5=(arr[mid-1]+arr[mid])/2
+    
+
+
+arr1=[]
+n1=int(input())
+for i in range(0,n1):
+    ele=int(input())
+    arr1.append(ele)
+arr1=sorted(arr1)
+mid=abs(int(len(arr1)/2))
+if n1%2==1:
+    arr6=arr1[mid]
+    
+else:
+    arr6=(arr1[mid-1]+arr1[mid])/2
+    
+
+
+arr2=[]
+n2=int(input())
+for i in range(0,n2):
+    ele=int(input())
+    arr2.append(ele)
+arr2=sorted(arr2)
+mid=floor((n2)/2)
+if n2%2==1:
+    arr7=arr2[mid]
+    
+else:
+    arr7=(arr2[mid-1]+arr2[mid])/2
+    
+print("\n")
+print(floor(arr5))
+print(floor(arr6))
+print(floor(arr7))
